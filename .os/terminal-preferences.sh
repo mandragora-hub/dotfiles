2597763f6b528d45451d23f083f09cf4925d6a1a
Royer Antonio Gomez Jimenez <rogomez@hacienda.gov.do>
2022-10-19 11:43:26 -0400

    :bug: Fix print error

--- .os/terminal-preferences.sh
+++ .os/terminal-preferences.sh
@@ -15,7 +15,7 @@ terminalprefs() {
 	print_info "Background color: rgb(46,52,54)."
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/background-color "'rgb(46,52,54)'"
 
-	print_info "Background transparency: 5%."
+	print_info "Background transparency: 5."
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/background-transparency-percent "'5'"
 
 	print_info "Font: MesloLGS NF Bold Italic. Size 10."
@@ -27,7 +27,7 @@ terminalprefs() {
 	print_info "Setting palette."
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/palette "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
 
-	print_info "Other terminal settings ."
+	print_info "Other terminal settings."
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/use-system-font "false"
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/use-theme-colors "false"
 	dconf write /org/gnome/terminal/legacy/profiles:/:"${DEFAULT_PROFILE}"/use-theme-transparency "false"
