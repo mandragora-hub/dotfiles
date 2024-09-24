177de8f01d91241fe1323b7d64130c7cd6351027
123 <123n>
2024-09-24 16:10:57 +0000

    save-progress

--- .zshrc
+++ .zshrc
@@ -1,3 +1,6 @@
+# Pokemon-colorscript => https://gitlab.com/phoneybadger/pokemon-colorscripts
+pokemon-colorscripts --no-title -r
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -5,6 +8,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+# 
 source ~/.aliases
 source ~/.functions
 source ~/.exports
---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Install tools 
+sudo apt update
+sudo apt install -y xclip bat neofetch stow jq vim
+
+# terminal preferences
+# TODO: Verify .os/terminal-preference.sh
+
+# Install Oh-My-ZSH, theme, and plugins 
+sudo apt update && sudo apt install -y zsh
+chsh -s /bin/zsh
+
+curl -L http://install.ohmyz.sh | sh &> /dev/null
+	
+echo 'Installing p10k theme'
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	
+
+# Node and Npm packages
+
+
+# Pokemon script
+git clone https://gitlab.com/phoneybadger/pokemon-colorscripts.git
+pushd pokemon-colorscripts
+sudo ./install.sh
+popd
+
+# Create symlink of dotfiles home directory
+stow -t ${HOME}
