494845504d17e4e23081fdcf92dc497c7441490b
Royer Gomez <royer01gomez@gmail.com>
2024-10-22 09:12:06 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,11 +32,13 @@ sudo apt install -y git xclip bat neofetch stow jq vim
 # terminal preferences
 # TODO: Verify .os/terminal-preference.sh
 
-# Install Oh-My-ZSH, theme, and plugins 
+# Install zsh, theme, and plugins 
 sudo apt update && sudo apt install -y zsh
 sudo chsh -s $(which zsh) $(whoami)
 
-curl -L http://install.ohmyz.sh | sh &> /dev/null
+# Install Oh-My-ZSH via curl
+echo 'Installing Oh-My-ZSH'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	
 echo 'Installing p10k theme'
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
